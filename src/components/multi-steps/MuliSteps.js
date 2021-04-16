0c7433c298339628c46ea292be07c704347857c7
marcelobarrera <darmis.barrera@computershare.co.uk>
2021-04-16 21:41:37 +0100

    multiform multisteps converted to functional component

--- src/components/multi-steps/MuliSteps.js
+++ src/components/multi-steps/MuliSteps.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+
 import Step1 from "./Step1";
 import Step2 from "./Step2";
 import StepResult from "./StepResult";
 
-class MultiSteps extends React.Component {
-  state = {
+const numberOfSteps = 2;
+const MultiSteps = () => {
+  const [state, setState] = useState({
     currentStep: 1,
     name: "",
     role: "",
@@ -12,83 +14,80 @@ class MultiSteps extends React.Component {
     password: "",
     receiveUpdate: true,
     receiveCommunication: false,
-  };
+  });
 
-  numberOfSteps = 2;
-
-  handleChange = (event) => {
+  const handleChange = (event) => {
     const { name, value } = event.target;
     console.log(name, value);
-    this.setState({
+    setState({
+      ...state,
       [name]: value,
     });
   };
 
-  handleChangeCheckbox = (event) => {
+  const handleChangeCheckbox = (event) => {
     const { name, checked } = event.currentTarget;
-    this.setState({
+    setState({
+      ...state,
       [name]: checked,
     });
   };
 
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    this.next();
-    console.log(JSON.stringify(this.state, null, 2));
+    next();
+    console.log(JSON.stringify(state, null, 2));
   };
 
-  next = () => {
-    const { currentStep } = this.state;
-    this.setState({
-      currentStep: currentStep + 1,
+  const next = () => {
+    setState({
+      ...state,
+      currentStep: state.currentStep + 1,
     });
   };
 
-  render() {
-    const { currentStep } = this.state;
-    return (
-      <React.Fragment>
-        <p>Step {this.state.currentStep} </p>
+  return (
+    <React.Fragment>
+      <p>Step {state.currentStep} </p>
 
-        <form onSubmit={this.handleSubmit}>
-          {currentStep === 1 && (
-            <Step1
-              handleChange={this.handleChange}
-              name={this.state.name}
-              email={this.state.email}
-              password={this.state.password}
-              role={this.state.role}
-            />
-          )}
-          {currentStep === 2 && (
-            <Step2
-              handleChange={this.handleChangeCheckbox}
-              receiveUpdate={this.state.receiveUpdate}
-              receiveCommunication={this.state.receiveCommunication}
-            />
-          )}
-          {currentStep === this.numberOfSteps + 1 && <StepResult />}
+      <form onSubmit={handleSubmit}>
+        {state.currentStep === 1 && (
+          <Step1
+            handleChange={handleChange}
+            name={state.name}
+            email={state.email}
+            password={state.password}
+            role={state.role}
+          />
+        )}
+        {state.currentStep === 2 && (
+          <Step2
+            handleChange={handleChangeCheckbox}
+            receiveUpdate={state.receiveUpdate}
+            receiveCommunication={state.receiveCommunication}
+          />
+        )}
+        {state.currentStep === numberOfSteps + 1 && <StepResult />}
 
-          {currentStep < this.numberOfSteps ? (
-            <button
-              className="btn btn-primary float-right"
-              type="button"
-              onClick={this.next}
-            >
-              Submit
-            </button>
-          ) : currentStep === this.numberOfSteps ? (
-            <input
-              type="submit"
-              className="btn btn-primary float-right"
-              value="Submit"
-              disabled={false}
-            />
-          ) : null}
-        </form>
-      </React.Fragment>
-    );
-  }
-}
+        {state.currentStep < numberOfSteps ? (
+          <button
+            className="btn btn-primary float-right"
+            type="button"
+            onClick={next}
+          >
+            Submit
+          </button>
+        ) : state.currentStep === numberOfSteps ? (
+          <input
+            type="submit"
+            className="btn btn-primary float-right"
+            value="Submit"
+            disabled={false}
+          />
+        ) : null}
+      </form>
+    </React.Fragment>
+  );
+};
 
 export default MultiSteps;
